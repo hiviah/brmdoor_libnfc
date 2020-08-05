374ba4df5dda230cfd23fccb4f4933898f7b4898
Ondrej Mikle <ondrej.mikle@gmail.com>
2020-08-05 20:34:01 +0200

    Option to import desfires

--- import_jendasap_cards.py
+++ import_jendasap_cards.py
@@ -5,18 +5,33 @@ import os
 import os.path
 import sqlite3
 
-from brmdoor_adduser import addUidAuth
+from brmdoor_adduser import addUidAuth, addNdefAuth
 from create_authenticator_db import createTables
 
+from optparse import OptionParser
+
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print "import_jendasap_cards.py <cards_from_sap.txt> <destination.sqlite>"
-        print "This will generate and COMPLETELY OVERWRITE the UID table in destination.sqlite"
+    parser = OptionParser()
+    parser.add_option("-d", "--desfire", dest="use_desfire",
+                      help="import Desfire table", action="store_true", default=False)
+
+    (options, args) = parser.parse_args()
+
+    if len(args) < 2:
+        print "import_jendasap_cards.py [--desfire] <cards_from_sap.txt> <destination.sqlite>"
+        print "This will generate and COMPLETELY OVERWRITE the UID/Desfire table in destination.sqlite"
         print "This is useful for initial import, but for individual cards use brmdoor_adduser.py"
         sys.exit(1)
 
-    destSqliteFname = sys.argv[2]
-    srcCardsFname = sys.argv[1]
+    if options.use_desfire:
+        destTable = "authorized_desfires"
+        importFunc = addNdefAuth
+    else:
+        destTable = "authorized_uids"
+        importFunc = addUidAuth
+
+    destSqliteFname = args[1]
+    srcCardsFname = args[0]
     nickUidList = []
     with file(srcCardsFname) as f:
         lineNo = 0
@@ -35,14 +50,14 @@ if __name__ == "__main__":
     conn = sqlite3.connect(destSqliteFname)
     cursor = conn.cursor()
     if dbExists:
-        cursor.execute("DELETE FROM authorized_uids")
+        cursor.execute("DELETE FROM %s" % destTable)
     else:
         createTables(cursor)
 
     for (nick, uid) in nickUidList:
-        addUidAuth(cursor, uid, nick)
+        importFunc(cursor, uid, nick)
 
     conn.commit()
     conn.close()
-    print "Converted %d nick-uid pairs into authorized_uids table" % (len(nickUidList),)
+    print "Converted %d nick-uid pairs into %s table" % (len(nickUidList), destTable)
 
